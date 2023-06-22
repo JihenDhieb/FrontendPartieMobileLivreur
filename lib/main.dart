be1411414ad554e63b38235b61ba9a900ddc8e39
jihen303 <129227172+jihen303@users.noreply.github.com>
2023-06-22 01:08:42 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'Welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'Welcome.dart';
 import 'Notification.dart';
 import 'LocationService.dart';
 
@@ -18,6 +18,29 @@ class MyApp extends StatelessWidget {
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
   final LocationService _locationService = LocationService();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> showNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id', // Replace with your own channel ID
+      'your_channel_name', // Replace with your own channel name
+      'your_channel_description', // Replace with your own channel description
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'notification_payload', // Replace with your own payload data
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +54,14 @@ class MyApp extends StatelessWidget {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       if (message.notification != null) {
         print("Received foreground message: ${message.notification!.title}");
+        showNotification(
+          message.notification!.title!,
+          message.notification!.body!,
+        );
       }
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      // Handle messages when the app is opened from a terminated state
       if (message.notification != null) {
         print(
             "Opened app from terminated state: ${message.notification!.title}");
@@ -49,6 +75,18 @@ class MyApp extends StatelessWidget {
     });
 
     FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: (String? payload) async {
+      if (payload != null) {
+        MyApp.navigatorKey.currentState
+            ?.pushReplacementNamed('/notification', arguments: payload);
+      }
+    });
 
     return MaterialApp(
       navigatorKey: navigatorKey, // Set the navigatorKey
